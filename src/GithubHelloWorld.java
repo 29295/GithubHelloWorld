3bf216b1243336e5cb9164ed786831e26248b1cd
yordanos <yordanos.negash.2021@student.lhschools.org>
2020-02-21 13:55:18 -0500

    called hello world method again

--- src/GithubHelloWorld.java
+++ src/GithubHelloWorld.java
@@ -1,14 +1,18 @@
 public class GithubHelloWorld {
     public static void main(String[] args) {
-      System.out.println("HelloWorld");
-       helloWorld();
-    }
+        System.out.println("HelloWorld");
+        helloWorld();
 
-    public static void helloWorld(){
+
+
+    }
+    public static void helloWorld() {
+
         System.out.println("Hello Github World");
     }
 
+    public static void YourName() {
 
-
-
+        System.out.println("this is yordanos  ");
+    }
 }
